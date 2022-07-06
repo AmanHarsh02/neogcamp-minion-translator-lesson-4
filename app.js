3a5cab9e0d147cb1cc171265b015888ff3fbc515
Aman Harsh <amanharsh0399@gmail.com>
2022-07-07 00:47:44 +0530

    feat: added minion translation API
    Added minion translation API and styled the web site with css.

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 var btnTranslate = document.querySelector("#btn-translate");
-var inputText = document.querySelector("#user-input");
+var userInput = document.querySelector("#user-input");
 var outputDiv = document.querySelector("#output-div")
 
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function constructUrl(text) {
+    return url + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error Occurred.", error)
+    alert("Something went wrong with server! Try again after some time.")
+}
+
+function fetchTranslatedText(text) {
+    fetch(constructUrl(text))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+            console.log(json)
+        })
+        .catch(errorHandler)
+}
+
 function clickHandler() {
-    outputDiv.innerText = inputText.value
+    var inputText = userInput.value;
+    fetchTranslatedText(inputText);
 }
 
 btnTranslate.addEventListener("click", clickHandler);
